68f223f5991cc2261385dc45152f921b79b18d9f
Aiji Uejima <aiji42@gmail.com>
2021-03-07 23:18:44 +0900

    down grade nextjs

--- apps/chat/next.config.js
+++ apps/chat/next.config.js
@@ -1,6 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx')
 
-module.exports = {
-  target: 'serverless'
-}
+module.exports = withNx({})
