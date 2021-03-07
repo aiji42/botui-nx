ea138da85b6e1006d0e0ac7c95fc1dc0cc8d9a37
Aiji Uejima <aiji42@gmail.com>
2021-03-07 22:40:25 +0900

    no use withNx

--- apps/chat/next.config.js
+++ apps/chat/next.config.js
@@ -1,6 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx')
 
-module.exports = withNx({
-  target: 'experimental-serverless-trace'
-})
+module.exports = {}
