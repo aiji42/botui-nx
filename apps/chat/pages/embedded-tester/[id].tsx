2e7a0cbb64537f6d8c267db78a32539e3b6d192e
Aiji Uejima <aiji42@gmail.com>
2021-04-02 00:16:44 +0900

    update embedded-tester

--- apps/chat/pages/embedded-tester/[id].tsx
+++ apps/chat/pages/embedded-tester/[id].tsx
@@ -1,26 +1,54 @@
-import React, { FC, useEffect } from 'react'
-import Botui from '@botui/embedded'
+import React, { FC, useEffect, useState } from 'react'
 import { GetServerSideProps } from 'next'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 interface TesterProps {
   id?: string
 }
 
+const script = (id: string, open: boolean) => `
+var call = function(src, handler){
+    var base = document.getElementsByTagName("script")[0];
+    var obj = document.createElement("script");
+    obj.async = true;
+    obj.src= src;
+    if(obj.addEventListener){
+        obj.onload = function () {
+        handler();
+        }
+    }else{
+        obj.onreadystatechange = function () {
+            if ("loaded" == obj.readyState || "complete" == obj.readyState){
+                obj.onreadystatechange = null;
+                handler();
+            }
+        }
+    }
+    base.parentNode.insertBefore(obj,base);
+};
+
+call("/api/script/chat.min.js", function(){new Botui.default("/?sessionId=${id}", ${open}).start()})
+`
+
 const Tester: FC<TesterProps> = ({ id }) => {
   const router = useRouter()
+  const [mounted, setMounted] = useState(false)
   useEffect(() => {
-    new Botui(`/?sessionId=${id ?? ''}`, Boolean(router.query.open)).start()
-  }, [id, router])
+    setMounted(true)
+  }, [])
   return (
-    <div>
+    <>
+      {mounted && <Head>
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: script(id ?? '', Boolean(router.query.open)) }} />
+      </Head>}
       <form id="testForm" method="post" action="/api/form-push-test">
         <input type="text" name="familyName" placeholder="familyName" />
         <input type="text" name="firstName" placeholder="firstName" />
         <input type="text" name="familyNameKana" placeholder="familyNameKana" />
         <input type="text" name="firstNameKana" placeholder="firstNameKana" />
       </form>
-    </div>
+    </>
   )
 }
 
