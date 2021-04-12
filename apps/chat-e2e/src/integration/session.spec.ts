51ad481b425d436721770cc4d3285a0e5448c2d3
Aiji Uejima <aiji42@gmail.com>
2021-04-13 00:10:27 +0900

    chat-e2e: add test for embbede-tester and update test config

--- apps/chat-e2e/src/integration/session.spec.ts
+++ apps/chat-e2e/src/integration/session.spec.ts
@@ -1,7 +1,7 @@
-describe('chat', () => {
-  beforeEach(() => cy.visit('/session/bfadcbd8-c4fd-471e-bf9a-418fab1d29c1'))
+describe('session', () => {
+  beforeEach(() => cy.visit('/session/9040a628-6afa-4874-982c-a6e1a8de877b', { timeout: 40000 }))
 
-  it('should be able to access', () => {
+  it('should be able to access main page', () => {
     cy.contains('いらっしゃいませ！')
     cy.contains('まずはお名前を教えて下さい。')
     cy.get('input[name="familyName"]').type('上島')
@@ -9,5 +9,6 @@ describe('chat', () => {
     cy.get('input[name="familyNameKana"]').type('うえじま')
     cy.get('input[name="firstNameKana"]').type('あいじ').blur()
     cy.get('button').last().click()
+    cy.contains('上島愛史様ですね。')
   })
 })
