490090224583d834dd755e96419935283578473e
Aiji Uejima <aiji42@gmail.com>
2021-05-02 22:09:06 +0900

    chore

--- apps/admin/src/components/Session/parts/ColorInput.tsx
+++ apps/admin/src/components/Session/parts/ColorInput.tsx
@@ -8,16 +8,6 @@ import {
   ClickAwayListener
 } from '@material-ui/core'
 import { useForm } from 'react-final-form'
-import {
-  StylesProvider,
-  createGenerateClassName
-} from '@material-ui/core/styles'
-
-// https://github.com/mikbry/material-ui-color/pull/142
-// https://github.com/mikbry/material-ui-color/pull/142/file
-const generateClassName = createGenerateClassName({
-  seed: 'ColorPicker'
-})
 
 const ColorInput: FC<InputProps<TextFieldProps>> = (props) => {
   const {
@@ -37,35 +27,33 @@ const ColorInput: FC<InputProps<TextFieldProps>> = (props) => {
   const handleClose = useCallback(() => setOpen(false), [])
 
   return (
-    <StylesProvider generateClassName={generateClassName}>
-      <Box display="flex" justifyContent="flex-start" position="relative">
-        <Box
-          component={Paper}
-          marginRight={1}
-          width={40}
-          height={40}
-          style={{ backgroundColor: value, cursor: 'pointer' }}
-          onClick={handleOepn}
-        />
-        <TextInputMU
-          name={name}
-          label={props.label}
-          onChange={onChange}
-          error={!!(touched && error)}
-          helperText={touched && error ? error : ' '}
-          required={isRequired}
-          value={value}
-          {...rest}
-        />
-        {open && (
-          <Box position="absolute" zIndex={1}>
-            <ClickAwayListener onClickAway={handleClose}>
-              <ChromePicker color={value} onChangeComplete={handleChange} />
-            </ClickAwayListener>
-          </Box>
-        )}
-      </Box>
-    </StylesProvider>
+    <Box display="flex" justifyContent="flex-start" position="relative">
+      <Box
+        component={Paper}
+        marginRight={1}
+        width={40}
+        height={40}
+        style={{ backgroundColor: value, cursor: 'pointer' }}
+        onClick={handleOepn}
+      />
+      <TextInputMU
+        name={name}
+        label={props.label}
+        onChange={onChange}
+        error={!!(touched && error)}
+        helperText={touched && error ? error : ' '}
+        required={isRequired}
+        value={value}
+        {...rest}
+      />
+      {open && (
+        <Box position="absolute" zIndex={1}>
+          <ClickAwayListener onClickAway={handleClose}>
+            <ChromePicker color={value} onChangeComplete={handleChange} />
+          </ClickAwayListener>
+        </Box>
+      )}
+    </Box>
   )
 }
 
