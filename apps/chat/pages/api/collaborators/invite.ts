9d1c45c86bad00b53072f036ea3ec40af507f9fe
Aiji Uejima <aiji42@gmail.com>
2021-05-28 23:59:23 +0900

    chat: update collaborator api

--- apps/chat/pages/api/collaborators/invite.ts
+++ apps/chat/pages/api/collaborators/invite.ts
@@ -3,10 +3,14 @@ import { NextApiHandler } from 'next'
 import API from '@aws-amplify/api'
 import dayjs from 'dayjs'
 import { v4 as uuidv4 } from 'uuid'
+import Amplify from 'aws-amplify';
+
+if (process.env.NEXT_PUBLIC_AWS_EXPORTS)
+  Amplify.configure(JSON.parse(process.env.NEXT_PUBLIC_AWS_EXPORTS))
 
 const invite: NextApiHandler = async (req, res) => {
   if (req.method !== 'POST') {
-    res.status(405).json({ message: '405 Method Not Allowed' })
+    res.status(405).json({ message: 'Method Not Allowed' })
     return
   }
   if (!req.body.sessionId || !req.body.email) {
@@ -29,6 +33,8 @@ const invite: NextApiHandler = async (req, res) => {
     console.log(result)
     res.status(500).json({ message: `Error` })
   }
+
+  res.status(200).json({ message: 'succeed' })
 }
 
 export default invite
