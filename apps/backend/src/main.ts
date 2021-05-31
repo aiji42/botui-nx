64b65db65bc1df70568cd6974484c320c25387b0
Aiji Uejima <aiji42@gmail.com>
2021-06-01 00:21:29 +0900

    backend: update api cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -10,6 +10,10 @@ import { AppModule } from './app/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept'
+  })
   const globalPrefix = 'api'
   app.setGlobalPrefix(globalPrefix)
   app.useGlobalPipes(new ValidationPipe())
