1b16d40add2595f9ffb279317c9bd9df03a5d943
Aiji Uejima <aiji42@gmail.com>
2021-06-14 23:57:16 +0900

    chat: add analytics (wip)

--- apps/chat/pages/_app.tsx
+++ apps/chat/pages/_app.tsx
@@ -1,10 +1,34 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect } from 'react'
 import { AppProps } from 'next/app'
 import { GlobalStyle } from '../components/GrobalStyle'
+import Head from 'next/head'
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const analyticsId = pageProps.session?.analyticsId
+  useEffect(() => {
+    analyticsId &&
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (window as any).gtag('config', analyticsId)
+  }, [analyticsId])
   return (
     <Fragment>
+      <Head>
+        {analyticsId && (
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${analyticsId}`}
+          />
+        )}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            `
+          }}
+        />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </Fragment>
