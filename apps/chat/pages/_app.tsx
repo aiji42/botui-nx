89b7c5b45640ca7a05636bbd39c239e459d7f921
Aiji Uejima <aiji42@gmail.com>
2021-06-15 22:54:13 +0900

    chat: add own gtag

--- apps/chat/pages/_app.tsx
+++ apps/chat/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { Fragment, useEffect } from 'react'
 import { AppProps } from 'next/app'
 import { GlobalStyle } from '../components/GrobalStyle'
-import Head from 'next/head'
 
 function CustomApp({ Component, pageProps }: AppProps) {
   const analyticsId = pageProps.session?.analyticsId
@@ -10,23 +9,6 @@ function CustomApp({ Component, pageProps }: AppProps) {
   }, [analyticsId])
   return (
     <Fragment>
-      <Head>
-        {analyticsId && (
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${analyticsId}`}
-          />
-        )}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            `
-          }}
-        />
-      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </Fragment>
