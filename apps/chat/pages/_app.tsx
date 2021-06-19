bd884b922e09098f05576e793380d20ac82ebece
Aiji Uejima <aiji42@gmail.com>
2021-06-19 16:04:23 +0900

    Revert "chat: update tracking dcript"
    This reverts commit e998dd8faeb7e668c928670de502b5e51a12fa40.

--- apps/chat/pages/_app.tsx
+++ apps/chat/pages/_app.tsx
@@ -5,12 +5,7 @@ import { GlobalStyle } from '../components/GrobalStyle'
 function CustomApp({ Component, pageProps }: AppProps) {
   const analyticsId = pageProps.session?.googleAnalyticsId
   useEffect(() => {
-    if (!analyticsId) return
-    const script = document.createElement('script')
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${analyticsId}`
-    script.async = true
-    document.head.appendChild(script)
-    gtag('config', analyticsId)
+    analyticsId && gtag('config', analyticsId)
   }, [analyticsId])
   return (
     <Fragment>
