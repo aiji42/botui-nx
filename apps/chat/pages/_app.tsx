e998dd8faeb7e668c928670de502b5e51a12fa40
Aiji Uejima <aiji42@gmail.com>
2021-06-19 15:27:00 +0900

    chat: update tracking dcript

--- apps/chat/pages/_app.tsx
+++ apps/chat/pages/_app.tsx
@@ -5,7 +5,12 @@ import { GlobalStyle } from '../components/GrobalStyle'
 function CustomApp({ Component, pageProps }: AppProps) {
   const analyticsId = pageProps.session?.googleAnalyticsId
   useEffect(() => {
-    analyticsId && gtag('config', analyticsId)
+    if (!analyticsId) return
+    const script = document.createElement('script')
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${analyticsId}`
+    script.async = true
+    document.head.appendChild(script)
+    gtag('config', analyticsId)
   }, [analyticsId])
   return (
     <Fragment>
