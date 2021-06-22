d526cb9d7127d117bdcd3dee4f29929ada892ac1
Aiji Uejima <aiji42@gmail.com>
2021-06-22 23:50:39 +0900

    admin: update AnalyticsTabInner

--- apps/admin/src/components/Session/Show/AnalyticsTabInner.tsx
+++ apps/admin/src/components/Session/Show/AnalyticsTabInner.tsx
@@ -1,38 +1,76 @@
 import { VFC, lazy, Suspense } from 'react'
 import { FunctionField } from 'react-admin'
 import { Session } from '@botui/types'
-import { Box, Typography, makeStyles } from '@material-ui/core'
+import { Box, Grid, Typography } from '@material-ui/core'
 
 const DateAreaChart = lazy(() => import('./AnalyticsChart/DateAreaChart'))
-
-const useStyles = makeStyles(() => ({
-  img: {
-    width: '100%',
-    maxWidth: 600
-  }
-}))
+const PassedCountBar = lazy(() => import('./AnalyticsChart/PassedCountBar'))
+const ProgressChart = lazy(() => import('./AnalyticsChart/ProgressChart'))
 
 export const AnalyticsTabInner: VFC = () => {
-  const classes = useStyles()
-
   return (
     <FunctionField<Session>
       source="id"
       label=""
-      render={() => (
-        <Box textAlign="center" m={1}>
-          <Typography variant="h5" color="secondary">
-            COMING SOON...
-          </Typography>
-          <Suspense fallback={null}>
-            <DateAreaChart
-              sessionId="9040a628-6afa-4874-982c-a6e1a8de877b"
-              begin="2021-06-19"
-              end="2021-06-22"
-            />
-          </Suspense>
-        </Box>
-      )}
+      render={(record) =>
+        record && (
+          <Grid container component={Box} textAlign="center" m={1}>
+            <Grid
+              item
+              xs={12}
+              component={Box}
+              pb={8}
+              height="400px"
+              width="100px"
+            >
+              <Typography>開始・完了カウンタ/CVR</Typography>
+              <Suspense fallback={null}>
+                <DateAreaChart
+                  sessionId={record.id}
+                  begin="2021-06-19"
+                  end="2021-06-23"
+                />
+              </Suspense>
+            </Grid>
+            <Grid
+              item
+              sm={12}
+              md={6}
+              component={Box}
+              pb={8}
+              height="320px"
+              width="100px"
+            >
+              <Typography>フォーム入力完了カウンタ</Typography>
+              <Suspense fallback={null}>
+                <PassedCountBar
+                  sessionId={record.id}
+                  begin="2021-06-19"
+                  end="2021-06-23"
+                />
+              </Suspense>
+            </Grid>
+            <Grid
+              item
+              sm={12}
+              md={6}
+              component={Box}
+              pb={8}
+              height="320px"
+              width="100px"
+            >
+              <Typography>進捗率カウンタ</Typography>
+              <Suspense fallback={null}>
+                <ProgressChart
+                  sessionId={record.id}
+                  begin="2021-06-19"
+                  end="2021-06-23"
+                />
+              </Suspense>
+            </Grid>
+          </Grid>
+        )
+      }
     />
   )
 }
