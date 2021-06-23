e3eb207d5b83833430169c33ede94b3129b8fa6b
Aiji Uejima <aiji42@gmail.com>
2021-06-23 23:43:23 +0900

    admin: create PassedCountBar

--- apps/admin/src/components/Session/Show/AnalyticsChart/PassedCountBar.tsx
+++ apps/admin/src/components/Session/Show/AnalyticsChart/PassedCountBar.tsx
@@ -8,6 +8,7 @@ import {
   ResponsiveContainer,
   LabelList
 } from 'recharts'
+import { Box, Typography } from '@material-ui/core'
 
 export interface Props {
   sessionId: string
@@ -16,7 +17,7 @@ export interface Props {
 }
 
 const PassedCountBar: VFC<Props> = ({ sessionId, begin, end }) => {
-  const [dataSet, setDataSet] = useState()
+  const [dataSet, setDataSet] = useState<null | Array<unknown>>(null)
   useEffect(() => {
     const query = new URLSearchParams({ sessionId, begin, end })
     fetch(
@@ -33,16 +34,33 @@ const PassedCountBar: VFC<Props> = ({ sessionId, begin, end }) => {
   }, [begin, end, sessionId])
 
   return (
-    <ResponsiveContainer height="100%">
-      <BarChart data={dataSet} layout="vertical">
-        <XAxis type="number" allowDecimals={false} />
-        <YAxis type="category" dataKey="passed" tick={false} />
-        <Tooltip />
-        <Bar dataKey="count" fill="#5aabf6">
-          <LabelList dataKey="passed" position="insideLeft" />
-        </Bar>
-      </BarChart>
-    </ResponsiveContainer>
+    <>
+      <ResponsiveContainer height="100%">
+        <BarChart data={dataSet} layout="vertical">
+          <XAxis type="number" allowDecimals={false} />
+          <YAxis type="category" dataKey="passed" tick={false} />
+          <Tooltip />
+          <Bar dataKey="count" fill="#5aabf6">
+            <LabelList dataKey="passed" position="insideLeft" />
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
+      {dataSet?.length < 3 && (
+        <Box
+          width="100%"
+          height="100%"
+          bgcolor="rgb(17 34 51 / 30%)"
+          top={0}
+          position="absolute"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h5">十分なデータが有りません</Typography>
+        </Box>
+      )}
+    </>
   )
 }
 
