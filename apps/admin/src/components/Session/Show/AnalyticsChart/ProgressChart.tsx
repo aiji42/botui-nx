cafba609aafecded6a0fe96fd6945bde6e74c739
Aiji Uejima <aiji42@gmail.com>
2021-06-23 23:44:57 +0900

    admin: update ProgressChart

--- apps/admin/src/components/Session/Show/AnalyticsChart/ProgressChart.tsx
+++ apps/admin/src/components/Session/Show/AnalyticsChart/ProgressChart.tsx
@@ -4,11 +4,11 @@ import {
   Area,
   Tooltip,
   CartesianGrid,
-  Line,
   YAxis,
   XAxis,
   ResponsiveContainer
 } from 'recharts'
+import { Box, Typography } from '@material-ui/core'
 
 export interface Props {
   sessionId: string
@@ -17,7 +17,7 @@ export interface Props {
 }
 
 const ProgressChart: VFC<Props> = ({ sessionId, begin, end }) => {
-  const [dataSet, setDataSet] = useState([])
+  const [dataSet, setDataSet] = useState<null | Array<unknown>>(null)
   useEffect(() => {
     const query = new URLSearchParams({ sessionId, begin, end })
     fetch(
@@ -34,27 +34,44 @@ const ProgressChart: VFC<Props> = ({ sessionId, begin, end }) => {
   }, [begin, end, sessionId])
 
   return (
-    <ResponsiveContainer height="100%">
-      <ComposedChart data={dataSet}>
-        <defs>
-          <linearGradient id="colorOpen" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#2196f3" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#4dabf5" stopOpacity={0} />
-          </linearGradient>
-        </defs>
-        <XAxis dataKey="progress" unit="%" />
-        <YAxis allowDecimals={false} />
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
-        <Area
-          dataKey="count"
-          type="monotone"
-          stroke="#4dabf5"
-          fillOpacity={1}
-          fill="url(#colorOpen)"
-        />
-      </ComposedChart>
-    </ResponsiveContainer>
+    <>
+      <ResponsiveContainer height="100%">
+        <ComposedChart data={dataSet}>
+          <defs>
+            <linearGradient id="colorOpen" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#2196f3" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#4dabf5" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="progress" unit="%" />
+          <YAxis allowDecimals={false} />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area
+            dataKey="count"
+            type="monotone"
+            stroke="#4dabf5"
+            fillOpacity={1}
+            fill="url(#colorOpen)"
+          />
+        </ComposedChart>
+      </ResponsiveContainer>
+      {dataSet?.length < 3 && (
+        <Box
+          width="100%"
+          height="100%"
+          bgcolor="rgb(17 34 51 / 30%)"
+          top={0}
+          position="absolute"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h5">十分なデータが有りません</Typography>
+        </Box>
+      )}
+    </>
   )
 }
 
