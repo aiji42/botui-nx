d9bb92b16228f59d790d6c95cb7886ff0b0bdaac
Aiji Uejima <aiji42@gmail.com>
2021-07-19 23:17:55 +0900

    Make it possible to retrieve data from getServerSideProps

--- apps/chat/pages/admin/index.tsx
+++ apps/chat/pages/admin/index.tsx
@@ -1,7 +1,19 @@
+import { createHasuraClient } from "../../libs/hasuraClient";
+import { GetServerSideProps } from "next";
 import { VFC } from "react";
 
-const Admin: VFC = () => {
+export const getServerSideProps: GetServerSideProps = async () => {
+  const hasuraClient = createHasuraClient()
+  const res = await hasuraClient.listScenarios({ email: 'aiji42@gmail.com' })
+  console.log(res)
 
+  return {
+    props: {}
+  }
+}
+
+const Admin: VFC = () => {
+  return null
 }
 
 export default Admin
